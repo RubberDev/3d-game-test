b3fc46f89f031ac83a143da76d5eeb5281291f11
MyNamesRubber <mullenissiah14@gmail.com>
2026-01-12 11:36:38 -0600

    Began work on settings, volume slider

--- Player/Scripts/pause_menu.gd
+++ Player/Scripts/pause_menu.gd
@@ -1,15 +1,17 @@
 extends Control
 
 func _ready() -> void:
-	self.hide()
+	$MainP.hide()
+	$Settings.hide()
 
 func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("Pause"):
 		if get_tree().paused == false:
-			self.show()
+			$MainP.show()
 			get_tree().paused = true
 		else:
-			self.hide()
+			$MainP.hide()
+			$Settings.hide()
 			get_tree().paused = false
 	
 	if Input.is_action_just_pressed("Pause"):
@@ -21,9 +23,19 @@ func _input(_event: InputEvent) -> void:
 # Resume game, close pause menu
 func _on_resume_pressed() -> void:
 	get_tree().paused = false
-	self.hide()
+	$MainP.hide()
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 # Quit game (Add confirmation later maybe)
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+# Settings button
+func _on_settings_pressed() -> void:
+	$Settings.show()
+
+# Volume
+var bus = AudioServer.get_bus_index("Master")
+func _on_volume_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(bus, linear_to_db(value))
+	$Settings/VBoxContainer/VolumeLabel.text = "Volume: " + str(value*100)
