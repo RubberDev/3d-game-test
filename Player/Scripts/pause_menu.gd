7451cfce63225a318b243e682d98a5b442f9acc7
MyNamesRubber <mullenissiah14@gmail.com>
2026-01-17 10:14:40 -0600

    Still working on it

--- Player/Scripts/pause_menu.gd
+++ Player/Scripts/pause_menu.gd
@@ -1,6 +1,6 @@
 extends Control
 
-var save_path = "res://settingsInfo.save"
+var save_path = "user://settingsInfo.save"
 
 func _ready() -> void:
 	$MainP.hide()
@@ -112,24 +112,24 @@ func _on_scaling_item_selected(index: int) -> void:
 		2:
 			RenderingServer.viewport_set_scaling_3d_mode(get_viewport().get_viewport_rid(), RenderingServer.VIEWPORT_SCALING_3D_MODE_BILINEAR)
 
-var test : int = 10
-
 # NEEDS TO BE FINISHED
 func save_info():
+	# Oh this'll be fun, I can tell
 	var file = FileAccess.open(save_path, FileAccess.WRITE)
-	file.store_var(test)
-	file.store_var(AudioServer.get_bus_volume_db(bus))
-	file.store_var(DisplayServer.window_get_mode())
-	file.store_var(get_viewport().msaa_3d)
-	file.store_var(get_viewport().use_taa)
+	file.store_var(AudioServer.get_bus_volume_db(bus), true)
+	file.store_var(DisplayServer.window_get_mode(), true)
+	file.store_var(get_viewport().msaa_3d, true)
+	file.store_var(get_viewport().use_taa, true)
 	#file.store_var(RenderingServer)
 	#file.store_var(RenderingServer.viewport_scaling)
 
 func load_info():
+	print("Test")
 	if FileAccess.file_exists(save_path):
 		var file = FileAccess.open(save_path, FileAccess.READ)
 		AudioServer.set_bus_volume_db(bus, linear_to_db(file.get_var(AudioServer.get_bus_volume_db(bus))))
 		DisplayServer.window_set_mode(file.get_var(DisplayServer.window_get_mode()))
+		
 		
 	else:
 		print("No save file detected")
